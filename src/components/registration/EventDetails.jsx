2e17962849c42f9a56287f11bb7d448cec00c4e8
Khristian Brooks <kh.brooks02@gmail.com>
2019-03-28 14:43:54 -0400

    change registration form style, edit event details

--- src/components/registration/EventDetails.jsx
+++ src/components/registration/EventDetails.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
-import { Grid, Col, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const EventDetails = ({ handleSubmit }) => (
-	<form onSubmit={handleSubmit}>
-		<Grid>
-			<Col xs={12} md={8} className="column col-md-offset-2">
-				<div className="chunk">
-					<h3 className="chunk">Event Details</h3>
-					<p>Friend-</p>
-					<p>
-						Giddy UP and register for the 2019 Youth Leader’s Summit! All roads
-						lead to BBQ, rodeos, and IYC planning. (If you require airport pick
-						up/drop off please be prepared to submit that information)
-					</p>
-					<p>-Xavius President & Executive Chairman</p>
-				</div>
-				<div>
-					<Button bsStyle="primary" type="submit" className="next">
-						Next
-					</Button>
-				</div>
-			</Col>
-		</Grid>
+	<form className="form-design" onSubmit={handleSubmit}>
+		<div className="chunk">
+			<h3 className="chunk">Event Details</h3>
+			<p>Friend-</p>
+			<p>
+				Giddy UP and register for the 2019 Youth Leader’s Summit! All roads lead
+				to BBQ, rodeos, and IYC planning. (If you require airport pick up/drop
+				off please be prepared to submit that information)
+			</p>
+			<p>-Xavius President & Executive Chairman</p>
+		</div>
+		<Button bsStyle="primary" type="submit" className="next">
+			Next
+		</Button>
 	</form>
 );
 export default reduxForm({
--- src/components/registration/PersonalInformation.jsx
+++ src/components/registration/PersonalInformation.jsx
@@ -3,165 +3,140 @@ import { Field, reduxForm } from 'redux-form';
 import { Grid, Col, ControlLabel, Button } from 'react-bootstrap';
 
 const PersonalInformation = ({ handleSubmit, handleBack }) => (
-	<form onSubmit={handleSubmit}>
-		<Grid className="row" fluid={true}>
-			<Col xs={12} md={8} className="column col-md-offset-2">
-				<div className="chunk">
-					<h3> Personal Information </h3>
-					<div>
-						<ControlLabel>Name</ControlLabel>
+	<form className="form-design" onSubmit={handleSubmit}>
+		<div className="chunk">
+			<h3> Personal Information </h3>
+			<div>
+				<ControlLabel>Name</ControlLabel>
+				<Field
+					name="name"
+					placeholder="John Doe"
+					className="form-control"
+					component="input"
+					type="text"
+					required
+				/>
+			</div>
+			<div>
+				<ControlLabel>Email</ControlLabel>
+				<Field
+					name="email"
+					placeholder="email@example.com"
+					className="form-control"
+					component="input"
+					type="text"
+					required
+				/>
+			</div>
+			<div>
+				<ControlLabel>Phone</ControlLabel>
+				<Field
+					name="phone"
+					placeholder="123-456-7890"
+					className="form-control"
+					component="input"
+					type="text"
+					required
+				/>
+			</div>
+			<div>
+				<ControlLabel>Mailing Address</ControlLabel>
+				<Field
+					name="mail"
+					placeholder="123 Branch Street"
+					className="form-control"
+					component="input"
+					type="text"
+					required
+				/>
+			</div>
+			<div>
+				<ControlLabel>Age</ControlLabel>
+				<div className="form-input">
+					<p>
 						<Field
-							name="name"
-							placeholder="John Doe"
-							className="form-control"
+							name="age"
 							component="input"
-							type="text"
-							required
-						/>
-					</div>
-					<div>
-						<ControlLabel>Email</ControlLabel>
+							type="radio"
+							value="Little Spark"
+						/>{' '}
+						Little Sparks (3-6)
+					</p>
+					<p>
 						<Field
-							name="email"
-							placeholder="email@example.com"
-							className="form-control"
+							name="age"
 							component="input"
-							type="text"
-							required
-						/>
-					</div>
-					<div>
-						<ControlLabel>Phone</ControlLabel>
+							type="radio"
+							value="Fireflies"
+						/>{' '}
+						Fireflies (7-12)
+					</p>
+					<p>
 						<Field
-							name="phone"
-							placeholder="123-456-7890"
-							className="form-control"
+							name="age"
 							component="input"
-							type="text"
-							required
-						/>
-					</div>
-					<div>
-						<ControlLabel>Mailing Address</ControlLabel>
+							type="radio"
+							value="Sparklers"
+						/>{' '}
+						Sparklers (13-17)
+					</p>
+					<p>
+						<Field name="age" component="input" type="radio" value="Ignitors" />{' '}
+						Ignitors (18-25)
+					</p>
+					<p>
 						<Field
-							name="mail"
-							placeholder="123 Branch Street"
-							className="form-control"
+							name="age"
 							component="input"
-							type="text"
-							required
-						/>
-					</div>
-					{/* 
-						<div>
-						<ControlLabel>Age</ControlLabel>
-						<div className="form-input">
-							<p>
-								<Field
-									name="age"
-									component="input"
-									type="radio"
-									value="Little Spark"
-								/>{' '}
-								Little Sparks (3-6)
-							</p>
-							<p>
-								<Field
-									name="age"
-									component="input"
-									type="radio"
-									value="Fireflies"
-								/>{' '}
-								Fireflies (7-12)
-							</p>
-							<p>
-								<Field
-									name="age"
-									component="input"
-									type="radio"
-									value="Sparklers"
-								/>{' '}
-								Sparklers (13-17)
-							</p>
-							<p>
-								<Field
-									name="age"
-									component="input"
-									type="radio"
-									value="Ignitors"
-								/>{' '}
-								Ignitors (18-25)
-							</p>
-							<p>
-								<Field
-									name="age"
-									component="input"
-									type="radio"
-									value="Fireworker"
-								/>{' '}
-								Fireworkers (25-39)
-							</p>
-							<p>
-								<Field
-									name="age"
-									component="input"
-									type="radio"
-									value="Campfire"
-								/>{' '}
-								Campfire (40+)
-							</p>
-						</div>
-					</div>
-				*/}
-					{/* 
-					<div>
-						<ControlLabel>Do You Have Any Allergies?</ControlLabel>
-						<Field
-							name="allergies"
-							className="form-control"
-							component="input"
-							type="text"
-						/>
-					</div>
-					*/}
-					{/* 
-					<div>
-						<ControlLabel>Gender</ControlLabel>
-						<div className="form-input">
-							<p>
-								<Field
-									name="gender"
-									component="input"
-									type="radio"
-									value="Female"
-								/>{' '}
-								Female{' '}
-							</p>
-							<p>
-								<Field
-									name="gender"
-									component="input"
-									type="radio"
-									value="Male"
-								/>{' '}
-								Male{' '}
-							</p>
-						</div>
-					</div>
-					*/}
+							type="radio"
+							value="Fireworker"
+						/>{' '}
+						Fireworkers (25-39)
+					</p>
+					<p>
+						<Field name="age" component="input" type="radio" value="Campfire" />{' '}
+						Campfire (40+)
+					</p>
 				</div>
-				<Col md={6} className="form-buttons">
-					<Button type="button" className="previous" onClick={handleBack}>
-						Previous
-					</Button>
-				</Col>
-				<Col md={6} className="form-buttons">
-					<Button bsStyle="primary" type="submit" className="next">
-						Next
-					</Button>
-				</Col>
-			</Col>
-		</Grid>
+			</div>
+			<div>
+				<ControlLabel>Do You Have Any Allergies?</ControlLabel>
+				<Field
+					name="allergies"
+					className="form-control"
+					component="input"
+					type="text"
+				/>
+			</div>
+			<div>
+				<ControlLabel>Gender</ControlLabel>
+				<div className="form-input">
+					<p>
+						<Field
+							name="gender"
+							component="input"
+							type="radio"
+							value="Female"
+						/>{' '}
+						Female{' '}
+					</p>
+					<p>
+						<Field name="gender" component="input" type="radio" value="Male" />{' '}
+						Male{' '}
+					</p>
+				</div>
+			</div>
+		</div>
+		<Col md={6} className="form-buttons">
+			<Button type="button" className="previous" onClick={handleBack}>
+				Previous
+			</Button>
+		</Col>
+		<Col md={6} className="form-buttons">
+			<Button bsStyle="primary" type="submit" className="next">
+				Next
+			</Button>
+		</Col>
 	</form>
 );
 

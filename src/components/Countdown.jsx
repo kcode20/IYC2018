c0ed0ea56c691f9edc55402fe593fe783f99a695
Khristian Brooks <kh.brooks02@gmail.com>
2019-03-30 23:09:02 -0400

    remove images, edit home page, add countdown

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
+import '../assets/styles/Countdown.css';
+
 /* Registration Form for the Youth Leaders Summit 2018 
 Todo: Find default for the branchname
 */
